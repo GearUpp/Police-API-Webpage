bb1d5191b43fc196873ba095fbc47f9ac14a3b89
vlad <138448988+GearUpp@users.noreply.github.com>
2024-09-08 16:54:46 +0100

    Selection box data extraction and Postcode API working

--- Police_Data.js
+++ Police_Data.js
@@ -1,14 +1,18 @@
 let postcode = ["S", "W", "1", "A", "2", "A", "A"];
-let PoliceAPI = "https://data.police.uk/api/"
-let PostcodeAPI = "api.postcodes.io/postcodes/"
+let PoliceAPI = "https://data.police.uk/api/"  //API for police data
+let PostcodeAPI = "api.postcodes.io/postcodes/" //API for getting postcode data
 var postcoderegx = /^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})$/;
-let data = "";
+let PostcodeData = ""; //Postcode.io API var to all postcode related data from the API
+let PoliceData = ""; // Police Force Get request for all forces names request var
+let ForceSelected = ""; //DropDown Selection var
 
+
+let test = "";
 async function query() {
     let outputRAW = await fetch(PoliceAPI + "forces");
 
     if (!outputRAW.ok) { throw new error("Unable to contact Police API"); };
-    data = await outputRAW.json();
+    PoliceData = await outputRAW.json();
 
     console.log("Data pulled for /forces ");
     //console.log(data);
@@ -18,7 +22,7 @@ async function query() {
 
 
 function populateDropMenu() {
-    data.forEach(force => {
+    PoliceData.forEach(force => {
         let createLI = document.createElement("li");
         let createA = document.createElement("a");
         createA.href = "#";
@@ -31,21 +35,34 @@ function populateDropMenu() {
 }
 query();
 
-
+//validates and returns either full lang&long queury string or a false based on given POSTCODE
 async function postcodeValidate(postcode_input) {
-    let postcode_data = await fetch("https://api.postcodes.io/postcodes/" + postcode_input );
-    data = await postcode_data.json();
-    console.log(postcode_data);
+    let postcode_data = await fetch("https://api.postcodes.io/postcodes/" + postcode_input);
+    PostcodeData = await postcode_data.json();
+    //console.log(PostcodeData);
+    if (PostcodeData.error) {
+        alert("Postcode " + postcode_input + " is invalid according to postcodes.io !");
+        return false;
+    } else {
+        //Returns formated lang and long for postcode ready to be inserted into any Police data API
+        return ("lat=" + toString(PostcodeData.status["latitude"]) + "&lng=" + toString(PostcodeData.status["longitude"]));
+    }
 }
 
+//Click will initiate all relevant functions to validate postcode, find lang & long of postcode, 
 document.querySelector("#Submit").addEventListener("click", function () {
-    input = document.getElementById("postcode").value;
+
+    let input = document.getElementById("postcode").value;
     if (input.match(postcoderegx)) {
-    postcodeValidate(input);
+        postcodeValidate(input);
     } else {
         alert("Invalid Postcode format. Example: 'NE14 7AA'");
     };
 
+});
+
+document.querySelector("ul").addEventListener('click', function () {
+    ForceSelected = this.target.innerHTML;
 });
 
 
