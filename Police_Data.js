e54673d1ad8e680baf57e0c6d54aae8ce0c9b3d3
vlad <138448988+GearUpp@users.noreply.github.com>
2024-10-27 17:16:33 +0000

    Fixed Button from doing defaultACtion

--- Police_Data.js
+++ Police_Data.js
@@ -4,9 +4,7 @@ let PostcodeAPI = "api.postcodes.io/postcodes/" //API for getting postcode data
 var postcoderegx = /^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})$/;
 let currentDate = (new Date().getFullYear() + "-" + (new Date().getMonth() - 1)); // Get month and Year only
 let PostcodeData = ""; //Postcode.io API var to all postcode related data from the API
-let PoliceData = ""; // Police Force Get request for all forces names request var
 let crimeList = "";
-let ForceSelected = ""; //DropDown Selection var
 let CrimeSelected = []; //DropDown Selection var [Name , url]
 let QueryResponse = ""; //Full data response from form
 
@@ -27,11 +25,9 @@ document.querySelector("input").addEventListener("click", function () {
 
 // calls function that auto populates dropdown menu
 async function query() {
-    let outputRAW = await fetch(PoliceAPI + "forces");
     let crimeRaw = await fetch(PoliceAPI + "crime-categories");
 
-    if (!outputRAW.ok || !crimeRaw.ok) { throw new error("Unable to contact Police API"); };
-    PoliceData = await outputRAW.json();
+    if (!crimeRaw.ok) { throw new error("Unable to contact Police API"); };
     crimeList = await crimeRaw.json();
 
 
@@ -55,15 +51,6 @@ function populateDropMenu() {
 
     });
 
-    PoliceData.forEach(force => {
-        let createLI = document.createElement("li");
-        let createA = document.createElement("a");
-        createA.href = "#";
-        //createA.href = force.id;
-
-        document.querySelector("#Force_Drop ul:last-child").appendChild(createLI).appendChild(createA).className = "dropdown-item";
-        document.querySelector(".dropdown-menu li:last-child a").innerHTML = force.name;
-    });
 }
 
 // Populates Dropdown menu
@@ -71,95 +58,79 @@ function populateDropMenu() {
 
 //validates and returns either full lang&long queury string or a false based on given POSTCODE
 async function postcodeValidate(postcode) {
-    if(postcode.test(postcoderegx)){
+    if (postcoderegx.test(postcode)) {
         var postcode_data = await fetch("https://api.postcodes.io/postcodes/" + postcode);
-    var postcodeRaw_data = await postcode_data.json();
-    PostcodeData = postcodeRaw_data;
+        var PostcodeData_Raw = await postcode_data.json();
+        console.log(PostcodeData_Raw)
+        if (PostcodeData_Raw.status == 200) {
+            //console.log("Valid postcode Data")
+            //Returns formated lang and long for postcode ready to be inserted into any Police data API
+            console.log("lat=" + PostcodeData_Raw.result.latitude + "&lng=" + PostcodeData_Raw.result.longitude);
+            PostcodeData = { "latitude": PostcodeData_Raw.result.latitude, "longitude": PostcodeData_Raw.result.longitude };
+            return PostcodeData;
+        } else {
 
-    if (PostcodeData.status == 200) {
-        console.log("Valid postcode Data")
-        //Returns formated lang and long for postcode ready to be inserted into any Police data API
-        console.log("lat=" + PostcodeData.result.latitude + "&lng=" + PostcodeData.result.longitude);
-        PostcodeData = [PostcodeData.result.latitude, PostcodeData.result.longitude];
-        return PostcodeData.result;
+            alert("Postcode " + postcode + " is invalid according to postcodes.io !");
+            return false
+
+        }
+
     } else {
-        
-        alert("Postcode " + postcode + " is invalid according to postcodes.io !");
-    }
-    
-    } else {
-        console.log("Invalid Postcode")
+        console.log("Invalid Postcode Format")
+        return false
     }
 
-    console.log(postcodeRaw_data)
-    
 }
 //validates and returns either full lang&long queury string or a false based on given POSTCODE
 
 
 
 //Click will initiate all relevant functions to validate postcode, find lang & long of postcode, 
-document.querySelector("#Submit").addEventListener("click", function () {
+document.querySelector("#Submit").addEventListener("click", async function (event) {
+    event.preventDefault(); // Prevents the default form submission behavior
 
     postcode = document.getElementById("postcode").value;
+    // Wait for the postcode validation to complete
+    PostcodeData = await postcodeValidate(postcode);
+    
+    if (PostcodeData) {
 
-    //Verifies either Postcode or Force is selected, then checks data is valid in both cases
-    if (postcodeValidate(postcode) || ForceSelected) {
-        if (PostcodeData) {
-            console.log("Valid postcode")
-
-        };
-        if (ForceSelected) {
-            //Searcg based on Police Force Only
-            console.log("Valid Force")
-
+        if (CrimeSelected[0]) {
+            await PoliceDataFetch(true);
+        } else {
+            await PoliceDataFetch(false);
         }
     } else {
-
-         alert("Select Force or type in a postcode in !!");
-    };
-
-
-
-
-
-
-
+        alert("Please provide a valid postcode!");
+    }
 });
 //Click will initiate all relevant functions to validate postcode, find lang & long of postcode, 
 
 
-// Identify Force and Crime selected
-document.querySelector("#Force_Drop ul").addEventListener('click', function (a) {
+// Identify  Crime selected
 
-    ForceSelected = a.target.innerHTML;
-    locationSelection("Searching in " + ForceSelected);
-});
 document.querySelector("#Crime_Drop ul").addEventListener('click', function (a) {
     CrimeSelected[0] = a.target.innerHTML;
     CrimeSelected[1] = a.target.id;
-    console.log("Crime Selected: " + CrimeSelected[0] + ", URL: " + CrimeSelected[1]);
+
 
 });
-// Identify Force and Crime selected
+// Identify Crime selected
 
 
+//All Police API Requests
+async function PoliceDataFetch(CrimeFlag) {
 
-//All Police API Asyn Functions
-async function PostcodeFetch() {
-    var PostcodeDataRaw = await PostcodeFetch(postcode);
-
-    PostcodeData = await PostcodeDataRaw;
-
-    if (!PostcodeData) {
-        console.log("Postcode Data fetch failed,possibly wrong postcode, unable to pull police data without it...");
-        return false;
-    }
-
-
-    var QueryResponseRAW = await fetch(PoliceAPI + "crimes-street/all-crime?lat=" + (PostcodeData[0].toString()) + "&lng=" + (PostcodeData[1].toString()) + "&date=" + currentDate);
+    if (CrimeFlag == true) {
+        console.log(PoliceAPI + "crimes-street/" + CrimeSelected[1] + "?&date=" + currentDate + "&lat=" + PostcodeData["latitude"].toString() + "&lng=" + PostcodeData["longitude"].toString())
+        var QueryResponseRAW = await fetch(PoliceAPI + "crimes-street/" + CrimeSelected[1] + "?&date=" + currentDate + "&lat=" + PostcodeData["latitude"].toString() + "&lng=" + PostcodeData["longitude"].toString());
+        
+    } else {
+        var QueryResponseRAW = await fetch(PoliceAPI + "crimes-street/all-crime?&date=" + currentDate + "&lat=" + PostcodeData["latitude"].toString() + "&lng=" + PostcodeData["longitude"].toString());
+    };
+    console.log("Done Fetching")
     QueryResponse = await QueryResponseRAW.json();
-    console.log("Police Data fetched");
     console.log(QueryResponse);
+    document.getElementById("output_area").innerHTML = QueryResponse;
 }
 //All Police API Asyn Functions
